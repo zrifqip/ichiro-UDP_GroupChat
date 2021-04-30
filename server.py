534fe4ccabfc29aad3eeb9f6fa77dc8afaa4dce7
Muhammad Zufarrifqi Prakoso <77829361+zrifqip@users.noreply.github.com>
2021-04-30 23:39:44 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import threading
+
 import socket
 
 LocalHost = '127.0.0.1'
@@ -6,19 +6,23 @@ port = 42069
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((LocalHost,port))
 print('Server is up and listening')
-message = server.recvfrom(1024)
-client_address = message[1]
-str1 = str(message[0].decode())
-server.sendto(f"Server: Welcome to the server {str1},may I help you?".encode(), client_address)
-def receive() :
+def getName() :
+    message = server.recvfrom(1024)
+    client_address = message[1]
+    str1 = str(message[0].decode())
+    server.sendto(f"Server: Welcome to the server {str1},may I help you?".encode(), client_address)
+    return str1
+def ServerMessage(nickname) :
     while True :
         try :
             message = server.recvfrom(1024)
-            print("{name}: {message}".format(name = str1, message = str(message[0].decode())))
+            print("{name}: {message}".format(name = nickname, message = str(message[0].decode())))
             reply = input('server : ')
             client_address = message[1]
             server.sendto(reply.encode(), client_address) 
         except :
             print("Client has left the server")
-        
-receive()
+            break
+if __name__ == "__main__"   :
+    Name = getName()
+    ServerMessage(Name)
