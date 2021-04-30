d9445d7a7a16cbc1fcc7ee9bd1ce1faa9082119c
Muhammad Zufarrifqi Prakoso <77829361+zrifqip@users.noreply.github.com>
2021-04-30 23:39:18 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,34 @@
 import socket
-import threading 
-nickname = input("Nickname: ").encode()
+ 
+nickname = input("Nickname : ").encode()
 localIP = '127.0.0.1'
 localPort = 42069
 ClientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-print('Connected to the server')
+
 
 def writemessage() :
     while True :
-        try : 
-            message = input('{}: '.format(str(nickname).decode("utf-8")))
+        try :
+            message = input('{}: '.format(str(nickname)))
             ClientSock.sendto(message.encode(), (localIP,localPort))
             reply = ClientSock.recvfrom(1024)
-            print("Server: {}".format(str(reply[0])))
+            print("Server: {}".format(str(reply[0].decode())))
         except :
-            print("Error!!")
+            print("Disconnected")
             ClientSock.close()
             break
-ClientSock.sendto(nickname, (localIP,localPort))
-message = ClientSock.recvfrom(1024)
-print(str(message[0].decode()))
-writemessage()
+def sendName():
+    try :
+        print('Connected to the server')
+        ClientSock.sendto(nickname, (localIP,localPort))
+        message = ClientSock.recvfrom(1024)
+        print(str(message[0].decode()))
+    except :
+        print("Error!!")
+        ClientSock.close()
+        exit
+
+if __name__ == "__main__" :
+    sendName()
+    writemessage()
+
